e1072d5f81d18bbbc17353c885c37ff54e6f6993
Chi-Feng Tsai <b07902123@ntu.edu.tw>
2018-12-30 11:42:50 +0800

    can build windows executable

---
+++ setup.py
@@ -0,0 +1,19 @@
+from cx_Freeze import setup, Executable
+
+import os.path
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+
+setup(
+    name = 'PhopongElecpong',
+    options = {'build_exe':
+        {'packages': ['pygame'],
+         'include_files':[
+            #os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
+            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),
+         ],
+        }
+    },
+    executables = [Executable('main.py', base='Win32GUI')]
+)
